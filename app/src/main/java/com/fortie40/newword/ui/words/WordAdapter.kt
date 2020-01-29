ce523408680132de3485d78c6cbfefe102e7f6af
0________4 <you@example.com>
2020-01-29 17:44:00 +0300

    Add filter for searchview

--- app/src/main/java/com/fortie40/newword/ui/words/WordAdapter.kt
+++ app/src/main/java/com/fortie40/newword/ui/words/WordAdapter.kt
@@ -2,15 +2,27 @@ package com.fortie40.newword.ui.words
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.fortie40.newword.R
 import com.fortie40.newword.databinding.WordLayoutBinding
+import com.fortie40.newword.helperfunctions.HelperFunctions
 import com.fortie40.newword.roomdatabase.WordModel
 
-class WordAdapter: ListAdapter<WordModel, WordAdapter.WordViewHolder>(WordDiffCallBack()) {
+class WordAdapter(): ListAdapter<WordModel, WordAdapter.WordViewHolder>(WordDiffCallBack()),
+    Filterable {
+
+    private lateinit var wOriginalList: List<WordModel>
+    private lateinit var wFilteredList: List<WordModel>
+
+    constructor(wordList: List<WordModel>): this() {
+        wOriginalList = wordList
+        wFilteredList = wordList
+    }
 
     class WordDiffCallBack: DiffUtil.ItemCallback<WordModel>() {
         override fun areItemsTheSame(oldItem: WordModel, newItem: WordModel): Boolean {
@@ -42,5 +54,32 @@ class WordAdapter: ListAdapter<WordModel, WordAdapter.WordViewHolder>(WordDiffCa
 
     override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
         holder.bind(getItem(position))
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(p0: CharSequence?): FilterResults {
+                val charString = p0.toString()
+
+                wFilteredList = if (charString.isEmpty()) {
+                    wOriginalList
+                } else {
+                    val filteredList = wOriginalList
+                        .filter { HelperFunctions.toLowerCase(it.wordLearned).contains(charString) ||
+                                HelperFunctions.toLowerCase(it.language).contains(charString) ||
+                                HelperFunctions.toLowerCase(it.meaning).contains(charString) }
+                        .toMutableList()
+                    filteredList
+                }
+                val filterResults = FilterResults()
+                filterResults.values = wFilteredList
+                return filterResults
+            }
+
+            @Suppress("UNCHECKED_CAST")
+            override fun publishResults(p0: CharSequence?, p1: FilterResults?) {
+                submitList(p1?.values as List<WordModel>)
+            }
+        }
     }
 }
--- app/src/main/java/com/fortie40/newword/ui/words/WordsFragment.kt
+++ app/src/main/java/com/fortie40/newword/ui/words/WordsFragment.kt
@@ -1,7 +1,9 @@
 package com.fortie40.newword.ui.words
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.*
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -9,6 +11,7 @@ import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.fortie40.newword.R
 import com.fortie40.newword.databinding.WordsFragmentBinding
+import com.fortie40.newword.helperfunctions.HelperFunctions
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.words_fragment.*
 import timber.log.Timber
@@ -18,6 +21,11 @@ class WordsFragment : Fragment() {
     private lateinit var wordsFragmentBinding: WordsFragmentBinding
     private lateinit var root: View
     private lateinit var viewModel: WordsViewModel
+    private lateinit var wordAdapter: WordAdapter
+    private lateinit var handler: Handler
+    private lateinit var r: Runnable
+
+    private var isInitialized: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -51,6 +59,35 @@ class WordsFragment : Fragment() {
         getWords()
     }
 
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        super.onPrepareOptionsMenu(menu)
+        val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        searchView.queryHint = getString(R.string.search_anything)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                Timber.i("Query Submitted")
+                return false
+            }
+
+            override fun onQueryTextChange(p0: String): Boolean {
+                wordAdapter.filter.filter(HelperFunctions.toLowerCase(p0))
+                isInitialized = true
+                handler = Handler()
+                r = Runnable { word_items.scrollToPosition(0) }
+                handler.postDelayed(r, 300)
+                return false
+            }
+
+        })
+    }
+
+    override fun onPause() {
+        if (isInitialized) {
+            handler.removeCallbacks(r)
+        }
+        super.onPause()
+    }
+
     private fun getWords() {
         swipe_to_refresh.isRefreshing = true
         viewModel.allProducts.observe(viewLifecycleOwner, Observer { words ->
@@ -65,7 +102,7 @@ class WordsFragment : Fragment() {
                     Timber.d("Word: ${word.wordLearned}")
                     Timber.d("***************************************************************")
                 }
-                val wordAdapter = WordAdapter()
+                wordAdapter = WordAdapter(words)
                 words.let { wordAdapter.submitList(it) }
                 word_items.layoutManager = LinearLayoutManager(activity)
                 word_items.adapter = wordAdapter
