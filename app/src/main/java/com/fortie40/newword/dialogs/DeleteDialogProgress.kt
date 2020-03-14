ab9e325413a0f31f99d29c04ae277a7603023e77
HP <you@example.com>
2020-03-14 16:08:33 +0300

    Minor update on DeleteDialogProgress

--- app/src/main/java/com/fortie40/newword/dialogs/DeleteDialogProgress.kt
+++ app/src/main/java/com/fortie40/newword/dialogs/DeleteDialogProgress.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDialogFragment
 import com.fortie40.newword.R
+import kotlinx.android.synthetic.main.delete_dialog_progress.view.*
 
 class DeleteDialogProgress: AppCompatDialogFragment() {
     @SuppressLint("InflateParams")
@@ -14,6 +15,9 @@ class DeleteDialogProgress: AppCompatDialogFragment() {
             val builder = AlertDialog.Builder(activity!!)
             val inflater = it!!.layoutInflater
             val view = inflater.inflate(R.layout.delete_dialog_progress, null)
+
+            view.percentage.text = getString(R.string._0, "29")
+            view.items.text = getString(R.string._1_1, "0", "20")
             builder.setView(view)
 
             builder.create()
