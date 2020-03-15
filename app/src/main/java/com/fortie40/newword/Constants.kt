5ce5c1e275746c3388b8f4235d8e68f14a502496
HP <you@example.com>
2020-03-15 11:37:34 +0300

    Add simulation of deleting all words

--- app/src/main/java/com/fortie40/newword/Constants.kt
+++ app/src/main/java/com/fortie40/newword/Constants.kt
@@ -11,3 +11,8 @@ const val DELETE_DIALOG_PROGRESS = "delete_dialog_progress"
 
 // key for passing bundle from wordsFragment to DeleteDialogProgress
 const val NUMBER_OF_ITEMS_TO_DELETE = "number_of_items_to_delete"
+
+// progress min and max for progress bar in delete_dialog_progress
+// progress is set in DeleteDialogProgress class
+const val PROGRESS_MIN = 0
+const val PROGRESS_MAX = 100
--- app/src/main/java/com/fortie40/newword/dialogs/DeleteDialogProgress.kt
+++ app/src/main/java/com/fortie40/newword/dialogs/DeleteDialogProgress.kt
@@ -3,13 +3,23 @@ package com.fortie40.newword.dialogs
 import android.annotation.SuppressLint
 import android.app.Dialog
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDialogFragment
 import com.fortie40.newword.NUMBER_OF_ITEMS_TO_DELETE
+import com.fortie40.newword.PROGRESS_MAX
+import com.fortie40.newword.PROGRESS_MIN
 import com.fortie40.newword.R
 import kotlinx.android.synthetic.main.delete_dialog_progress.view.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class DeleteDialogProgress: AppCompatDialogFragment() {
+class DeleteDialogProgress : AppCompatDialogFragment() {
+    private lateinit var dView: View
     private var args: Bundle? = null
     private var numberOfItemsToDelete: Int = 0
 
@@ -20,13 +30,44 @@ class DeleteDialogProgress: AppCompatDialogFragment() {
             numberOfItemsToDelete = args!!.getInt(NUMBER_OF_ITEMS_TO_DELETE)
             val builder = AlertDialog.Builder(activity!!)
             val inflater = it!!.layoutInflater
-            val view = inflater.inflate(R.layout.delete_dialog_progress, null)
+            dView = inflater.inflate(R.layout.delete_dialog_progress, null)
 
-            view.percentage.text = getString(R.string._0, "0")
-            view.items.text = getString(R.string._1_1, "0", numberOfItemsToDelete.toString())
-            builder.setView(view)
+            dView.percentage.text = getString(R.string._0, 0)
+            dView.items.text = getString(R.string._1_1, 0, numberOfItemsToDelete)
+            dView.progress_bar.progress = PROGRESS_MIN
+            dView.progress_bar.max = PROGRESS_MAX
+            builder.setView(dView)
 
             builder.create()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        CoroutineScope(IO).launch {
+            deleteWords()
+            closeDialog()
+        }
+    }
+
+    private suspend fun deleteWords() {
+        delay(500)
+        for (i in 1..numberOfItemsToDelete) {
+            val progress = ((i.toFloat() / numberOfItemsToDelete) * 100).toInt()
+            withContext(Main) {
+                dView.percentage.text = getString(R.string._0, progress)
+                dView.items.text = getString(R.string._1_1, i, numberOfItemsToDelete)
+                dView.progress_bar.progress = progress
+            }
+            delay(3000)
+        }
+        delay(500)
+    }
+
+    private suspend fun closeDialog() {
+        withContext(Main) {
+            dialog?.dismiss()
+            dialog?.cancel()
         }
     }
 }
